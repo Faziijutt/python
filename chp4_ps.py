839b7bdb0b8896bf1fc40c0ab5f22c79bb17ea86
Faziijutt <juttfazii815@gmail.com>
2025-02-27 03:21:00 -0800

    first commit

--- chp4_ps.py
+++ chp4_ps.py
@@ -1,46 +1,57 @@
-# f=[]
+# problem 1
 
-# a=input("enter fruit name:")
-# f.append(a)
-# b=input("enter fruit name:")
-# f.append(b)
-# c=input("enter fruit name:")
-# f.append(c)
-# d=input("enter fruit name:")
-# f.append(d)
-# e=input("enter fruit name:")
-# f.append(e)
-# g=input("enter fruit name:")
-# f.append(g)
-# h=input("enter fruit name:")
-# f.append(h)
-# print(f)
+f=[]
 
-# f=[]
+a=input("enter fruit name:")
+f.append(a)
+b=input("enter fruit name:")
+f.append(b)
+c=input("enter fruit name:")
+f.append(c)
+d=input("enter fruit name:")
+f.append(d)
+e=input("enter fruit name:")
+f.append(e)
+g=input("enter fruit name:")
+f.append(g)
+h=input("enter fruit name:")
+f.append(h)
+print(f)
 
-# a=int(input("enter student marks:"))
-# f.append(a)
-# b=int(input("enter student marks:"))
-# f.append(b)
-# c=int(input("enter student marks:"))
-# f.append(c)
-# d=int(input("enter student marks:"))
-# f.append(d)
-# e=int(input("enter student marks:"))
-# f.append(e)
-# g=int(input("enter student marks:"))
-# f.append(g)
-# h=int(input("enter student marks:"))
-# f.append(h)
-# f.sort()
-# print(f)
+# problem 2
 
-# a=(34,0.454,"Faizan")
-# a[2]="Riaz"
-# print(a)
+f=[]
 
-# a=(2,4,3,1)
-# print(sum(a))
+a=int(input("enter student marks:"))
+f.append(a)
+b=int(input("enter student marks:"))
+f.append(b)
+c=int(input("enter student marks:"))
+f.append(c)
+d=int(input("enter student marks:"))
+f.append(d)
+e=int(input("enter student marks:"))
+f.append(e)
+g=int(input("enter student marks:"))
+f.append(g)
+h=int(input("enter student marks:"))
+f.append(h)
+f.sort()
+print(f)
+
+# problem 3
+
+a=(34,0.454,"Faizan")
+a[2]="Riaz"
+print(a)
+
+# problem 4
+
+a=(2,4,3,1)
+print(sum(a))
+
+# problem 5
+
 
 a=(1,4,0,7,0,6,0,0,5)
 print(a.count(0))
