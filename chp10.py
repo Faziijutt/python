a01111d621d6857571607f74a8b2e5b60846bc28
Faziijutt <juttfazii815@gmail.com>
2025-04-03 07:33:46 -0700

    first commit

---
+++ chp10.py
@@ -0,0 +1,13 @@
+class student:
+    fee=20000
+    sub="physics"
+    gender="male"
+f=student()
+f.name="faizan"
+print(f"Name: {f.name}\n Fee: {f.fee}\n Subject: {f.sub}\n Gender: {f.gender}")
+
+f.name="umair"
+print(f"Name: {f.name}\n Fee: {f.fee}\n Subject: {f.sub}\n Gender: {f.gender}")
+
+f.name="baker"
+print(f"Name: {f.name}\n Fee: {f.fee}\n Subject: {f.sub}\n Gender: {f.gender}")
