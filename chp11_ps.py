6ee2bda2f2d99202dc3efb259e6b3383c8acaddd
Faziijutt <juttfazii815@gmail.com>
2025-04-10 02:13:13 -0700

    first commit

--- chp11_ps.py
+++ chp11_ps.py
@@ -32,15 +32,35 @@
 # print(c.increment)
 
 
-class complex:
-    def __init__(self,r,i):
-        self.r=r
-        self.i=i
-    def __add__(self,c2):
-        return complex(self.r + c2.r,self.i + c2.i)
-    def __str__(self):
-        return f"{self.r} + {self.i}i"
-c1=complex(2,4)    
-c2=complex(5,6)    
-print(c1 + c2)
+# class complex:
+#     def __init__(self,r,i):
+#         self.r=r
+#         self.i=i
+#     def __add__(self,c2):
+#         return complex(self.r + c2.r,self.i + c2.i)
+#     def __str__(self):
+#         return f"{self.r} + {self.i}i"
+# c1=complex(2,4)    
+# c2=complex(5,6)    
+# print(c1 + c2)
 
+
+
+class vector:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.y=y
+    def __add__(self,other):
+        return vector(self.x+other.x,self.x+other.x,self.z+other.z) 
+    def __mul__(self,other):
+        return vector(self.x*other.x,self.x*other.x,self.z*other.z) 
+    def __str__(self):
+        return f"vector{self.x} {self.y} {self.z}"
+
+v1=vector(1,2,3)    
+v2=vector(4,5,6)    
+v3=vector(7,8,9)
+
+print(v1+v2)
+print(v2*v3)
